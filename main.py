21c1bf5d2ebff9e10addae1ce88ec0712e01c1b2
Theis Andatu <45439030+antheiz@users.noreply.github.com>
2023-09-20 15:00:42 +0700

    fix: Update main.py

--- main.py
+++ main.py
@@ -20,23 +20,23 @@ def gauss_jordan(A, B):
 # SPL 1
 print("SPL 1:")
 A1 = np.array([[1, 1, 1], [1, 2, -1], [2, 1, 2]], float)
-B1 = np.array([6, 2, 10])
+B1 = np.array([6, 2, 10], float)
 gauss_jordan(A1, B1)
 
 # SPL 2
 print("\nSPL 2:")
 A2 = np.array([[1, 1], [2, 4]], float)
-B2 = np.array([3, 8])
+B2 = np.array([3, 8], float)
 gauss_jordan(A2, B2)
 
 # SPL 3
 print("\nSPL 3:")
 A3 = np.array([[3.7, 4, 0], [6, 2, 1], [2, -1, -3]], float)
-B3 = np.array([45, 25, -15])
+B3 = np.array([45, 25, -15], float)
 gauss_jordan(A3, B3)
 
 # SPL 4
 print("\nSPL 4:")
 A4 = np.array([[2, 3, 1], [2, 1, 2], [3, 1, 2]], float)
-B4 = np.array([9, 9, 11])
+B4 = np.array([9, 9, 11], float)
 gauss_jordan(A4, B4)
